631494eed2161d3fc733df124a4e875511f4a0bc
myslas <baseballkid-88@D-10-154-252-59.dhcp4.washington.edu>
2014-12-04 13:10:07 -0800

    test

--- final.php
+++ final.php
@@ -1,5 +1,15 @@
 <?php
-	$db = pg_connect("dbname=yslasm user=yslasm password=mikematt44");
+   $host        = "host=127.0.0.1";
+   $port        = "port=5432";
+   $dbname      = "dbname=yslasm";
+   $credentials = "user=yslasm password=mikematt44";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 ?>
 
 <html>
