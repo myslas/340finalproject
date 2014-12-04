cf8b3ca60302b2f1ea86d8cb5e586c2bdf01e86a
myslas <baseballkid-88@D-10-154-252-59.dhcp4.washington.edu>
2014-12-04 12:50:45 -0800

    test

--- final.php
+++ final.php
@@ -1,25 +1,12 @@
-<?
-	$db = pg_connect("host=localhost port=5432 dbname=yslasm user=yslasm password=mikematt44");
-	$agencies = $db->query('SELECT * FROM agencies');
+<?php
+	$db = pg_connect("host=localhost port=5432 dbname=yslasm password=mikematt44");
 ?>
-<html> 
-	<body> 
-		<h1>Agencies</h1> 
-			<table border="1" width="100%"> 
-			<tr>
-			<th>Id</th> 
-			<th>Product</th> 
-			<th>Description</th> 
-			<th>Price</th> 
-			</tr>
-			
-			<tr> 
-			<td>@row.Id</td> 
-			<td>@row.Name</td> 
-			<td>@row.Description</td> 
-			<td style="text-align:right">@row.Price</td> 
-			</tr> 
-			}
-		</table> 
-	</body> 
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hello</h1>
+</body>
 </html>
